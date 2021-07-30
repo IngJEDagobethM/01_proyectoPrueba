ef81bf3cedcced3394a024511938e912d42e7310
Ing.JEDagobethM <x.game19@gmail.com>
2021-07-30 09:17:13 -0500

    se mejora aplicando swich

--- piedra_papel_tijeras.js
+++ piedra_papel_tijeras.js
@@ -4,34 +4,50 @@ const Tijeras = "tijeras";
 
 function valida_eleccion(eleccion, eleccion_maq){
     let resultado = "";
-    if (eleccion == Piedra) {
-        if (eleccion_maq == Papel){
-            resultado = eleccion+" pierde contra "+eleccion_maq;
-        }else if (eleccion_maq == Tijeras){
-            resultado = eleccion+" gana contra "+eleccion_maq;
-        }else{
-            resultado = "empate";
-        }
-    }else if (eleccion == Papel){
-        if (eleccion_maq == Tijeras){
-            resultado = eleccion+" pierde contra "+eleccion_maq;
-        }else if (eleccion_maq == Piedra){
-            resultado = eleccion+" gana contra "+eleccion_maq;
-        }else{
-            resultado = "empate";
-        }
-    }else if (eleccion == Tijeras){
-        if (eleccion_maq == Piedra){
-            resultado = eleccion+" pierde contra "+eleccion_maq;
-        }else if (eleccion_maq == Papel){
-            resultado = eleccion+" gana contra "+eleccion_maq;
-        }else{
-            resultado = "empate";
-        }
-    }else {
-        resultado = eleccion+" no es una elección valida.";
+    switch (eleccion) {
+        case Piedra:
+            switch (eleccion_maq){
+                case Papel:
+                    resultado = eleccion+" pierde contra "+eleccion_maq;
+                    break;
+                case Tijeras:
+                    resultado = eleccion+" gana contra "+eleccion_maq;
+                    break;
+                default:
+                    resultado = "empate";
+            }
+            break;
+        case Papel:
+            switch (eleccion_maq) {
+                case Tijeras:
+                    resultado = eleccion+" pierde contra "+eleccion_maq;
+                    break;
+                case Piedra:
+                    resultado = eleccion+" gana contra "+eleccion_maq;
+                    break;
+                default:
+                    resultado = "empate";
+            }
+            break;
+        case Tijeras:
+            switch (eleccion_maq){
+                case Piedra:
+                    resultado = eleccion+" pierde contra "+eleccion_maq;
+                    break;
+                case Papel:
+                    resultado = eleccion+" gana contra "+eleccion_maq;
+                    break;
+                default:
+                    resultado = "empate";
+            }
+            break;
+        default:
+            resultado = eleccion+" no es una elección valida.";
     }
     return resultado;
 }
 
 console.log(valida_eleccion(Papel,Tijeras));
+console.log(valida_eleccion(Piedra,Tijeras));
+console.log(valida_eleccion(Piedra,Papel));
+console.log(valida_eleccion(Tijeras,Tijeras));
