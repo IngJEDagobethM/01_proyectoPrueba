4ce4e0833bf7e20dbdba9ad254fe288f69853535
Ing.JEDagobethM <x.game19@gmail.com>
2021-07-30 09:28:30 -0500

    se mejora aplicando condicional ternario

--- piedra_papel_tijeras.js
+++ piedra_papel_tijeras.js
@@ -4,45 +4,22 @@ const Tijeras = "tijeras";
 
 function valida_eleccion(eleccion, eleccion_maq){
     let resultado = "";
-    switch (eleccion) {
-        case Piedra:
-            switch (eleccion_maq){
-                case Papel:
-                    resultado = eleccion+" pierde contra "+eleccion_maq;
-                    break;
-                case Tijeras:
-                    resultado = eleccion+" gana contra "+eleccion_maq;
-                    break;
-                default:
-                    resultado = "empate";
-            }
-            break;
-        case Papel:
-            switch (eleccion_maq) {
-                case Tijeras:
-                    resultado = eleccion+" pierde contra "+eleccion_maq;
-                    break;
-                case Piedra:
-                    resultado = eleccion+" gana contra "+eleccion_maq;
-                    break;
-                default:
-                    resultado = "empate";
-            }
-            break;
-        case Tijeras:
-            switch (eleccion_maq){
-                case Piedra:
-                    resultado = eleccion+" pierde contra "+eleccion_maq;
-                    break;
-                case Papel:
-                    resultado = eleccion+" gana contra "+eleccion_maq;
-                    break;
-                default:
-                    resultado = "empate";
-            }
-            break;
-        default:
-            resultado = eleccion+" no es una elección valida.";
+    if (eleccion == eleccion_maq){
+        resultado = "empate";
+    }else{
+        switch (eleccion) {
+            case Piedra:
+                resultado = eleccion_maq == Tijeras ? eleccion+" gana contra "+eleccion_maq : eleccion+" pierde contra "+eleccion_maq;
+                break;
+            case Papel:
+                resultado = eleccion_maq == Piedra ? eleccion+" gana contra "+eleccion_maq : eleccion+" pierde contra "+eleccion_maq;
+                break;
+            case Tijeras:
+                resultado = eleccion_maq == Papel ? eleccion+" gana contra "+eleccion_maq : eleccion+" pierde contra "+eleccion_maq;
+                break;
+            default:
+                resultado = eleccion+" no es una elección valida.";
+        }
     }
     return resultado;
 }
